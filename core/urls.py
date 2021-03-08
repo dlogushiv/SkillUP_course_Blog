6a8193bc03c06c9a5c4081633044cc759704c193
dlogushiv <dlogushiv@gmail.com>
2021-03-08 05:17:21 +0200

    Lesson 5.8 - views changes via classes and create templates pages+css

--- core/urls.py
+++ core/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('home/', views.home, name='home_page'),
-    path('about/', views.about, name='about_page'),
-    path('test/', views.test, name='test_page'),
+    path('about/', views.AboutView.as_view(), name='about_page'),
+    path('home/', views.HomeView.as_view(), name='home_page'),
+    # path('test/', views.test, name='test_page'),
 ]
--- core/views.py
+++ core/views.py
@@ -1,18 +1,46 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.generic import TemplateView, ListView
+from .models import Post, Comment
+
+# bad practice to use functions
+
+# def home(request):
+#     return HttpResponse('<h1>Home page!</h1>'
+#                         '<a href="/about/">About</a>')
+#
+#
+# def about(request):
+#     return HttpResponse('<h1>About page!!</h1>'
+#                         '<a href="/home/">Home</a>'
+#                         '<br></br>'
+#                         '<a href="/test/">test</a>')
+#
+#
+# def test(request):
+#     return render(request, './frontend/index.html', {})
+
+# better is to use classes 'Classy Class-Based Views.' https://ccbv.co.uk
+
+posts = [
+    {'title': 'How to learn Python', 'author': 'admin', 'data': '12345678'},
+    {'title': 'How to learn Java', 'shy': 'admin', 'data': '124587963'},
+    {'title': 'How to learn cmd', 'author': 'admin', 'data': '224857566'},
+]
 
 
-def home(request):
-    return HttpResponse('<h1>Home page!</h1>'
-                        '<a href="/core/about/">About</a>')
+class AboutView(TemplateView):
+    """ Base About view """
+    template_name = 'core/about.html'
 
 
-def about(request):
-    return HttpResponse('<h1>About page!!</h1>'
-                        '<a href="/core/home/">Home</a>'
-                        '<br></br>'
-                        '<a href="/core/test/">test</a>')
+class HomeView(ListView):
+    """ Base Home view """
+    template_name = 'core/home.html'
+    # queryset = [1, 2, 3]
+    # queryset = Post.objects.all()
+    # queryset = Post.objects.values()
+    queryset = Post.objects.values('title', 'body')
 
-
-def test(request):
-    return render(request, './frontend/index.html', {})
+    def get(self, request):
+        return super().get(request)
