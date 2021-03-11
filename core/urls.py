c4d2382f9fb8127c83464fcf1c7e6540cc49c408
dlogushiv <dlogushiv@gmail.com>
2021-03-11 21:48:34 +0200

    Lesson 5.9 - changed pages urls with pages names

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,17 @@
 from django.urls import path
 from . import views
 
+# if we have many applications, it is better to use namespases
+# create variable 'app_name = <application name>'
+# and after that use <a href="{% url 'app_name:page_name' %}">link text</a>
+# in template pages
+app_name = 'core'
+
 urlpatterns = [
+    path('', views.HomeView.as_view(), name='home_page'),
     path('about/', views.AboutView.as_view(), name='about_page'),
-    path('home/', views.HomeView.as_view(), name='home_page'),
     # path('test/', views.test, name='test_page'),
 ]
+# if we have not many applications/pages, we can not to use namespases
+# no need a variable 'app_name = <application name>'
+# and use <a href="{% url 'page_name' %}">link text</a> # in template pages
