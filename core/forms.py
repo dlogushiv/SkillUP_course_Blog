ddf47fe516f79542f9d457ab87055f41d9920e07
dlogushiv <dlogushiv@gmail.com>
2021-03-19 14:55:37 +0200

    Lesson 5.11 - add image field to form with safe upload to DB; some changes in html/css for better view ))

--- core/forms.py
+++ core/forms.py
@@ -5,7 +5,16 @@ from .models import Post, Comment
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
-        fields = ['author', 'title', 'body', ]
+        fields = ['author', 'title', 'body', 'image', ]
+
+    # methods clean use for custom data validation
+    # def clean_title(self):
+    #     if 'Python' not in self.data['title']:
+    #         raise forms.ValidationError("Can't create post without Python!")
+    #     return self.data['title']
+    #
+    # def clean(self, **kwargs):
+    #     return super(self).clean(**kwargs)
 
 
 class CommentForm(forms.ModelForm):
